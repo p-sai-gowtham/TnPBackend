c3b23a1a754d66f9c6647ea5d28f563a72b3eea0
Sai Gowtham P <puvvulasaigowtham@gmail.com>
2024-10-28 19:44:29 +0530

    update settings

--- TandP/urls.py
+++ TandP/urls.py
@@ -22,7 +22,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('home.urls')),
-    path('admin/', admin.site.urls),
     path('user/',include('user.urls'),name='user')
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
